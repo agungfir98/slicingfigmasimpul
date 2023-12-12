c508fc715f8268cf3b638cceb761b4f5db569b3c
Agung Firmansyah <agungfir98@gmail.com>
2023-12-12 13:31:11 +0700

    initialize projects

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					blue: "var(--main)",
+					background: "var(--background-color)",
+					gray: "var(--bg-second)",
+					"light-grey": "var(--bg-third)",
+				},
+				secondary: {
+					orange: "var(--tint-orange)",
+					blue: "var(--tint-blue)",
+					red: "var(--tint-red)",
+					yellow: "var(--tint-yellow)",
+				},
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
